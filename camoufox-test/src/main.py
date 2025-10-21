1e6417f10c48edbbd7a15c7307dd9ff77cc9a0a4
Cory Parsnipson <cparsnipson@gmail.com>
2025-10-20 23:10:22 -0700

    [camoufox-test] Add some debug messages

--- camoufox-test/src/main.py
+++ camoufox-test/src/main.py
@@ -10,10 +10,17 @@ logging.basicConfig(level=logging.INFO, format = "[%(levelname)s] %(message)s")
 
 if __name__ == "__main__":
     logging.info("Hello world!")
-    breakpoint()
 
     with Camoufox() as browser:
         page = browser.new_page()
+
+        logging.info(f"Created new browser page...")
+        logging.info(f"UserAgent = {page.evaluate('navigator.userAgent')}")
+        logging.info(f"Screen offset = {page.evaluate('window.screenX')}, {page.evaluate('window.screenY')}")
+        logging.info(f"Screen size = {page.evaluate('window.outerWidth')}, {page.evaluate('window.outerHeight')}")
+
+        breakpoint()
+
         diagnostics.run_stealth_diagnostic(page)
 
         breakpoint()
